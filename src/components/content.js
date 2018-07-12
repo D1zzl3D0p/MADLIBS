931b1f66fa59ce12cc14036fde02b10fea97adbc
DevClass <support-2@ahsmail.com>
2018-07-12 09:23:40 -0600

    Kinda Final

---
+++ src/components/content.js
@@ -0,0 +1,14 @@
+import React { Component } from 'react';
+
+class Content extends Component {
+    render(){
+        return(
+            <div clasNmae='content'>
+
+            </div>
+
+        )
+    }
+}
+
+export default Content;
